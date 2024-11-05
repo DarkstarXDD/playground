2a43cd202f03e5c526604dde6a5af57b75bc1e6d
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2024-11-05 16:32:56 +0530

    add my branch

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "/src/styles.css"
 export default function App() {
   return (
     <main>
-      <h1 className="text-3xl">Hello, World!</h1>
+      <h1 className="text-3xl">This is a test branch!</h1>
     </main>
   )
 }
