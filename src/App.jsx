5ec67fcca7872534e6f7a537e8273f6e141b467b
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2024-11-05 16:41:53 +0530

    update branch

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "/src/styles.css"
 export default function App() {
   return (
     <main>
-      <h1 className="text-3xl">This is a test branch!</h1>
+      <h1 className="text-3xl">This is a test branch - Updated!</h1>
     </main>
   )
 }
